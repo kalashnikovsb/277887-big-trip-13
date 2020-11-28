6f648826c2c2c89594c2cdbb8fc62c84b02db704
kalashnikovsb <kalashnikov.sk8@gmail.com>
2020-11-29 01:05:35 +0700

    Исправления

--- src/utils/commonUtils.js
+++ src/utils/commonUtils.js
@@ -4,24 +4,24 @@ const getRandomInteger = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const getRandomArrayElement = (array) => {
+const getRandomElement = (array) => {
   const lastIndex = array.length - 1;
   const randomIndex = getRandomInteger(0, lastIndex);
   return array[randomIndex];
 };
 
 // Функция возвращает подмассив переданного массива, имеющий случайную длину (в т.ч. пустой) и состоящий из случайных элементов
-const getRandomSubArray = (array, minLength, maxLength) => {
+const getRandomItems = (array, minLength, maxLength) => {
   let length = getRandomInteger(minLength, maxLength);
   if (length === 0) {
     return [];
   }
   let subArray = [];
   while (length) {
-    subArray.push(getRandomArrayElement(array));
+    subArray.push(getRandomElement(array));
     length--;
   }
   return subArray;
 };
 
-export {getRandomInteger, getRandomArrayElement, getRandomSubArray};
+export {getRandomInteger, getRandomElement, getRandomItems};

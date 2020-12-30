d4fab79ad5300449adc80280c606f1f8dd54f736
kalashnikovsb <kalashnikov.sk8@gmail.com>
2020-12-30 23:20:07 +0700

    Опечатка

--- src/view/event-edit-view.js
+++ src/view/event-edit-view.js
@@ -146,7 +146,7 @@ const getEventEditTemplate = (data) => {
         <div class="event__type-wrapper">
           <label class="event__type  event__type-btn" for="event-type-toggle-1">
             <span class="visually-hidden">Choose event type</span>
-            <img class="event__type-icon" width="17" height="17" src="img/icons/${getType(type)}.png" alt="Event type icon">
+            <img class="event__type-icon" width="17" height="17" src="img/icons/${getType(type).toLowerCase()}.png" alt="Event type icon">
           </label>
           <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
           ${getEventTypesList(type)}

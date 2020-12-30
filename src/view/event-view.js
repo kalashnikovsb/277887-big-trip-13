e2ea757c1283166f17db964c805cabdf7e4e8df4
kalashnikovsb <kalashnikov.sk8@gmail.com>
2020-12-30 23:14:59 +0700

    Опечатка

--- src/view/event-view.js
+++ src/view/event-view.js
@@ -46,7 +46,7 @@ const getEventTemplate = (event) => {
     <div class="event">
       ${getDateTemplate(timeStart)}
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
       </div>
       <h3 class="event__title">${type} ${destination}</h3>
       ${getSchedule(timeStart, timeEnd)}

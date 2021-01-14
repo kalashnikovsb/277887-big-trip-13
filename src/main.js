dbddb90b9715ed043f8a74b119e4a283f81a22bf
kalashnikovsb <kalashnikov.sk8@gmail.com>
2021-01-14 23:26:40 +0700

    Починил добавление нового эвента

--- src/main.js
+++ src/main.js
@@ -8,13 +8,56 @@ import FilterModel from "./model/filter-model.js";
 import StatisticView from "./view/statistic-view.js";
 import Api from "./api.js";
 
-const AUTHORIZATION = `Basic bF9cd7jfN8cP2qk6h`;
-const END_POINT = `https://13.ecmascript.pages.academy/big-trip`;
-
 const tripHeaderElement = document.querySelector(`.trip-main`);
 const tripEventsElement = document.querySelector(`.trip-events`);
 const menuHeaderElement = document.querySelector(`.trip-main__trip-controls .visually-hidden:nth-of-type(1)`);
 const filtersHeaderElement = document.querySelector(`.trip-main__trip-controls .visually-hidden:nth-of-type(2)`);
+
+const AUTHORIZATION = `Basic bF9cd7jfN8cP2qk6h`;
+const END_POINT = `https://13.ecmascript.pages.academy/big-trip`;
+
+const menuClickHandler = (menuItem) => {
+  if (currentMenuItem === menuItem) {
+    return;
+  }
+  currentMenuItem = menuItem;
+
+  switch (menuItem) {
+    case MenuItem.TABLE:
+      if (statisticComponent !== null) {
+        statisticComponent.hide();
+      }
+      tripPresenter.show();
+      filterPresenter.filtersEnable();
+      break;
+    case MenuItem.STATS:
+      if (statisticComponent !== null) {
+        statisticComponent.hide();
+      }
+      statisticComponent = new StatisticView(tripEventsElement, eventsModel.getEvents(), eventsModel.getOptions());
+      tripPresenter.hide();
+      statisticComponent.show();
+      filterPresenter.filtersDisable();
+      break;
+  }
+};
+
+const addNewEventClickHandler = (evt) => {
+  evt.preventDefault();
+  tripPresenter.show();
+  tripPresenter.createEvent();
+  currentMenuItem = MenuItem.TABLE;
+  menuComponent.setMenuItem(MenuItem.TABLE);
+
+  filterPresenter.filtersEnable();
+  if (statisticComponent !== null) {
+    statisticComponent.hide();
+  }
+};
+
+const menuComponent = new MenuView();
+render(menuHeaderElement, menuComponent, RenderPosition.AFTEREND);
+menuComponent.setMenuClickHandler(menuClickHandler);
 
 const eventsModel = new EventsModel();
 const filterModel = new FilterModel();
@@ -45,53 +88,16 @@ api.getOptions().then((options) => {
   eventsModel.setDestinations([]);
 });
 
-const tripPresenter = new TripPresenter(tripHeaderElement, tripEventsElement, eventsModel, filterModel, api);
-const filterPresenter = new FilterPresenter(filtersHeaderElement, filterModel, eventsModel);
-
-const menuComponent = new MenuView();
-render(menuHeaderElement, menuComponent, RenderPosition.AFTEREND);
+let currentMenuItem = MenuItem.TABLE;
+let statisticComponent = null;
 
 const addNewEventButton = document.querySelector(`.trip-main__event-add-btn`);
+addNewEventButton.addEventListener(`click`, addNewEventClickHandler);
+// Блокирую кнопку на время загрузки данных
 addNewEventButton.setAttribute(`disabled`, ``);
 
-addNewEventButton.addEventListener(`click`, (evt) => {
-  evt.preventDefault();
-  menuClickHandler(MenuItem.ADD_NEW_EVENT);
-});
-
-
-let statisticComponent = null;
-let currentMenuItem = MenuItem.TABLE;
-
-const menuClickHandler = (menuItem) => {
-  if (currentMenuItem === menuItem) {
-    return;
-  }
-  currentMenuItem = menuItem;
-
-  switch (menuItem) {
-    case MenuItem.ADD_NEW_EVENT:
-      statisticComponent.hide();
-      tripPresenter.show();
-      menuComponent.setMenuItem(menuItem);
-      tripPresenter.createEvent(menuClickHandler);
-      filterPresenter.filtersEnable();
-      break;
-    case MenuItem.TABLE:
-      statisticComponent.hide();
-      tripPresenter.show();
-      filterPresenter.filtersEnable();
-      break;
-    case MenuItem.STATS:
-      statisticComponent = new StatisticView(tripEventsElement, eventsModel.getEvents(), eventsModel.getOptions());
-      tripPresenter.hide();
-      statisticComponent.show();
-      filterPresenter.filtersDisable();
-      break;
-  }
-};
-
-menuComponent.setMenuClickHandler(menuClickHandler);
+const tripPresenter = new TripPresenter(tripHeaderElement, tripEventsElement, eventsModel, filterModel, api);
+const filterPresenter = new FilterPresenter(filtersHeaderElement, filterModel, eventsModel);
 
 filterPresenter.init();
 tripPresenter.init();
